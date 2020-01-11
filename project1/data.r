b1b86e9f580a1fa13fa9f065bf223f0cc2262f46
Oskar Den Store <s183917@student.dtu.dk>
2020-01-11 12:54:24 +0100

    Lidt for at se på dataen

---
+++ project1/data.r
@@ -0,0 +1,27 @@
+rm(list=ls())
+load(file = "armdata.RData")
+data = armdata ; rm(armdata)
+
+par(mfrow=c(2,5))
+
+#For den første liste i dataen 
+ploti = F ; 
+for (i in 1:10){
+x <- ((data[[1]])[1])[[1]][[i]]
+if (ploti == T){
+  plot(x,add=T)
+}}
+
+#Lidt test af dataen 
+y=0
+rm(x) & rm(y)
+  x <- ((data[[1]])[1])[[1]][[1]] ; y <- ((data[[1]])[1])[[1]][[2]]
+  par(mfrow=c(1,1))
+  plot(x[,1],y[,1]) 
+  
+#Eksperimenter:
+x[,1]
+#Perons: 
+x[,2]
+#Repetitions:
+x[,3]
