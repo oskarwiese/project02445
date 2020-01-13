511c2b74644a9a4096198c2c4fce655d86e3f18d
Anders Henriksen <s183904@outlook.dk>
2020-01-13 12:13:21 +0100

    småændringer

--- project2/phosphordata.R
+++ project2/phosphordata.R
@@ -7,18 +7,20 @@ data = Phosphorous ; rm(Phosphorous)
 data$location <- as.factor(data$location)
 
 
-plot(data)
-par(mfrow = c(1,2))
-plot(data$DGT, data$yield, col = data$location, xlab = "DGT [mikrog/L]", ylab = "Yield [hkg/ha]")
-plot(data$olsenP, data$yield, col = data$location, xlab = "olsenP [mg/hg]", ylab = "Yield [hkg/ha]")
-plot(data$olsenP, data$DGT, col = data$location, xlab = "olsenP [mg/hg]", ylab = "DGT [mikrog/L]")
-
-
-fit1 <- lm(data$yield ~ data$DGT)
+fit1 <- lm(data$yield ~ log(data$DGT))
 summary(fit1)
 
-fit2 <- lm(data$yield ~ data$olsenP)
+fit2 <- lm(data$yield ~ log(data$olsenP))
 summary(fit2)
 
+
+plot(data)
+par(mfrow = c(1,2))
+plot(log(data$DGT), data$yield, col = data$location, xlab = expression(paste("DGT [",mu, "g/L]")), ylab = "Yield [hkg/ha]")
 curve(fit1$coefficients[1] + x * fit1$coefficients[2], add = T)
+plot(log(data$olsenP), data$yield, col = data$location, xlab = "olsenP [mg/hg]", ylab = "Yield [hkg/ha]")
 curve(fit2$coefficients[1] + x * fit2$coefficients[2], add = T)
+plot(data$olsenP, data$DGT, col = data$location, xlab = "olsenP [mg/hg]", ylab = expression(paste("DGT [", mu, "g/L]")))
+
+
+expression(paste("Sampled values, ", mu, "=5, ", sigma, "=1")))
