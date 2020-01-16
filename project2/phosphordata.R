0bb433c9aadf910b20d97532a6051c5a77c65419
Anders Henriksen <s183904@outlook.dk>
2020-01-16 10:51:14 +0100

    Fix af plots og results+intro

--- project2/phosphordata.R
+++ project2/phosphordata.R
@@ -8,16 +8,21 @@ data$location <- as.factor(data$location)
 
 # Linear fits 
 fit1 <- lm(data$yield ~ log(data$DGT))
-summary(fit1)
+summary(fit1) # r^2 = 0.4239
 
 fit2 <- lm(data$yield ~ log(data$olsenP))
-summary(fit2)
+summary(fit2) # r^2 = 0.1188
 
 par(mfrow = c(1,2))
 plot(log(data$DGT), data$yield, col = data$location, xlab = expression(paste("DGT [",mu, "g/L]")), ylab = "Yield [hkg/ha]")
 curve(fit1$coefficients[1] + x * fit1$coefficients[2], add = T)
+legend("bottomright", legend=c(expression(paste( r^2, " = 0.4239"))),
+       col=c("black"), cex=0.8)
+
 plot(log(data$olsenP), data$yield, col = data$location, xlab = "olsenP [mg/hg]", ylab = "Yield [hkg/ha]")
 curve(fit2$coefficients[1] + x * fit2$coefficients[2], add = T)
+legend("bottomright", legend=c(expression(paste( r^2, " = 0.1188"))),
+       col=c("black"), cex=0.8)
 
 #Boxplot af dataen
 par(mfrow = c(1,1))
@@ -39,6 +44,8 @@ summary(modelDGT)
 coef(modelDGT)
 plot(data$DGT, data$yield, col = data$location, xlab = expression(paste("DGT [",mu, "g/L]")), ylab = "Yield [hkg/ha]")
 curve(coef(modelDGT)[1] * x/(coef(modelDGT)[2] + x),from=2,to=170, add= T)
+legend("bottomright", legend=c("MSE = 10.84"),
+       col=c("black"), cex=0.8)
 
 # Fit non-linear model til olsenP og vis fit
 modelolsenP <- nls(yield ~ alfa * olsenP/(beta + olsenP), data = data, start = list(alfa = 90 , beta = 1))
@@ -46,6 +53,8 @@ summary(modelolsenP)
 coef(modelolsenP)
 plot(data$olsenP, data$yield, col = data$location, xlab = "olsenP [mg/hg]", ylab = "Yield [hkg/ha]")
 curve(coef(modelolsenP)[1] * x/(coef(modelolsenP)[2] + x), from=1, to=10, add= T)
+legend("bottomright", legend=c("MSE = 14.65"),
+       col=c("black"), cex=0.8)
 
 #Resdual plots af DGT og olsenP michalis-menten modellerne
 plot(resid(modelDGT))
